9c872ae16debe646a93db4e0e83fd8214015beab
Areeq Hasan <areeq.i.hasan@gmail.com>
2023-08-04 21:21:35 -0400

    something

---
+++ utils.py
@@ -0,0 +1,6 @@
+def latex_array(a):
+    lines = str(a).replace('[', '').replace(']', '').splitlines()
+    rv = [r'\begin{bmatrix}']
+    rv += ['  ' + ' & '.join(l.split()) + r'\\' for l in lines]
+    rv +=  [r'\end{bmatrix}']
+    return '\n'.join(rv)
